f89e8d369e5798103fad0c980b5ed2a9a4c08c18
AbdulBuilds2u <abdulghania614@gmai.com>
2026-09-16 21:54:24 +0500

    day1

--- main.dart
+++ main.dart
@@ -1,3 +1,59 @@
-void main(){
-    print ('hello world');
+//sales team management system
+
+void main(){  
+
+    String teamName = "Team1";
+    String teamHead = "Head.Ahad";
+      
+      List employee = [
+        "1.Ahmed",
+        "2.Saleem",
+        "3.Arshad",
+        "4.Salman",
+        "5.Huzaifa",
+        "6.shahid",
+        "7.Adeel",
+        "8.Majeed",
+        "9.Usman",
+        "10.Talha",
+        "11.Rehan",
+        "12.Hasnain",
+        "13.Muzammil",
+        "14.Shiraz",
+        "15.Daniyal",
+      ];
+    
+
+
+
+        print(teamName);
+  print("   $teamHead");
+
+  for (String employe in employee) {
+    print(" employ  $employe");
+  }
+
+class Employee {
+  int id = team 3001
+  String name = "1.Ahmed";
+  double salary = 20,000;
+  double commission = '20%';
+  String designation = sales executive;
+  int teamId = Team1;
+  double totalSales = commission + salary;
+  int leaves = 2;
+  int deductionperleaves = 1000;
+  Employee({
+    required this.id,
+    required this.name,
+    required this.salary,
+    required this.designation,
+    required this.teamId,
+    this.totalSales = 0,
+    this.leaves = 0,
+  });
 }
+
+
+
+}  
