6fb6ae0b58b76dc1df3cc8f36018eb8f0dbfdef4
AbdulBuilds2u <abdulghania614@gmai.com>
2026-09-17 20:48:54 +0500

    thursday

--- main.dart
+++ main.dart
@@ -2,59 +2,72 @@
 
 void main(){  
 
-    String teamName = "Team1";
-    String teamHead = "Head.Ahad";
+    
+    String teamlead = "Ahad";
       
       List employee = [
-        "1.Ahmed",
-        "2.Saleem",
-        "3.Arshad",
-        "4.Salman",
-        "5.Huzaifa",
-        "6.shahid",
-        "7.Adeel",
-        "8.Majeed",
-        "9.Usman",
-        "10.Talha",
-        "11.Rehan",
-        "12.Hasnain",
-        "13.Muzammil",
-        "14.Shiraz",
-        "15.Daniyal",
+        "1 Ahmed",
+        "2 Saleem",
+        "3 Arshad",
+        "4 Salman",
+        "5 Huzaifa",
       ];
     
 
-
-
-        print(teamName);
-  print("   $teamHead");
-
+     
+  print(" teamlead :  $teamlead");
+  
   for (String employe in employee) {
     print(" employ  $employe");
   }
 
-class Employee 1 {
-  int id = team 3001
-  String name = "1.Ahmed";
-  double salary = 20,000;
-  double commission = '20%';
-  String designation = sales executive;
-  int teamId = Team1;
-  double totalSales = commission + salary;
-  int leaves = 2;
-  int deductionperleaves = 1000;
-  Employee({
-    required this.id,
-    required this.name,
-    required this.salary,
-    required this.designation,
-    required this.teamId,
-    this.totalSales = 0,
-    this.leaves = 0,
-  });
-  
+Team ();
+employe();
 }
+    void Team (){
+int salesteam = 5;
+int teamlead = 1;
+int manager = 1;
+int totalstaff = 7;
+
+print ("Sales team contains on : ($salesteam) employee");
+print ("team lead = $teamlead");
+print ("manager = $manager");
+print ("total staff = $totalstaff");
+
+
+}
+   
+       
+void employe(){
+  int employes = 8;
+  if (employes>=4){
+    print ('present');
+    }
+    else {
+      print ('not all');
+    }
+}
+//   }
+//   int 1 = "Ahmed";
+//   int 2 = "Saleem";
+//   int 3 = "Arshad";
+//   int 4 = "Salman";
+//   int 5 = "huzaifa";
+// }
 
 
 
-}  
+
+  // int id = team 3001
+  // String name = "1.Ahmed";
+  // double salary = 20,000;
+  // double commission = '20%';
+  // String designation = sales executive;
+  // int teamId = Team1;
+  // double totalSales = commission + salary;
+  // int leaves = 2;
+  // int deductionperleaves = 1000;
+ 
+//sales team management system
+
